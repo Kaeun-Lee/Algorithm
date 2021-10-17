65ff713064fa1460d196df884eae68ce909dde81
devkani <lgeun7026@hanmail.net>
2021-10-17 23:34:38 +0900

    [LeetCode] 1528. Shuffle String

--- LeetCode/1528. Shuffle_String.py
+++ LeetCode/1528. Shuffle_String.py
@@ -1,9 +1,18 @@
 # https://leetcode.com/problems/shuffle-string/
 
 # Solution_1
-class Solution:
+class Solution:  
     def restoreString(self, s: str, indices: List[int]) -> str:
-        A = [''] * len(s)
+        A = [''] * len(s)  # 문자열 길이만큼 자리 만들기
         for i, c in zip(indices, s):
             A[i] = c
-        return "".join(A)
+        return ''.join(A)
+    
+
+# Solution_2
+class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str:
+        res[''] * len(s)
+        for i, v in enumerate(indices):
+            res[v] = s[i]
+        return ''.join(s)
