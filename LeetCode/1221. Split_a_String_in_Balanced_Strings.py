157d834099bafc6df31fe53884c175d27c0e2ac0
devkani <lgeun7026@hanmail.net>
2021-11-03 23:57:26 +0900

    [LeetCode] 1221. update other solutions

--- LeetCode/1221. Split_a_String_in_Balanced_Strings.py
+++ LeetCode/1221. Split_a_String_in_Balanced_Strings.py
@@ -16,3 +16,33 @@ class Solution:
                 cnt += 1
 
         return cnt
+
+
+# Solution_2 -> 속도 빠름
+from itertools import accumulate as acc  # list(acc([1,2,3])) -> [1, 3, 6]
+
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        return list(acc(1 if c == 'L' else -1 for c in s)).count(0)  # 리스트 중 0의 개수
+    
+
+# Solution_3
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        res = cnt = 0         
+        for c in s:
+            cnt += c == 'L'  # True + True = 2
+            cnt -= c == 'R'
+            res += cnt == 0
+        return res  
+    
+    
+# Solution_4
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        res = cnt = 0         
+        for c in s:
+            cnt += 1 if c == 'L' else -1            
+            if cnt == 0:
+                res += 1
+        return res  
