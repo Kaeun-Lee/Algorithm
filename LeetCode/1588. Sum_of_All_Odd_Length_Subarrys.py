f308dc0d336c5c390e3ee8ffca0541bb50aae91d
devkani <lgeun7026@hanmail.net>
2021-11-15 23:49:37 +0900

    [LeetCode] 1588. Update another solution

--- LeetCode/1588. Sum_of_All_Odd_Length_Subarrys.py
+++ LeetCode/1588. Sum_of_All_Odd_Length_Subarrys.py
@@ -1,11 +1,17 @@
 # https://leetcode.com/problems/sum-of-all-odd-length-subarrays/
 
 # Solution_1
-
-def sumOddLengthSubarrays(self, A):
-    n = len(A)
-    res = 0
-    for l in xrange(1, n + 1, 2):
-        for i in xrange(n - l + 1):
-            res += sum(A[i:i + l])
-    return res
+class Solution:
+    def sumOddLengthSubarrays(self, arr: List[int]) -> int:
+        n = len(arr)
+        res = 0
+        for l in range(1, n + 1,2):     # l = 1, 3, 5.. 
+            for i in range(n - l + 1):  # 0-5, 0-3, 0-1
+                res += sum(arr[i:i + l])  # 0:1, 1:2.. / 0:3, 1:4.. / 0:5
+        return res
+    
+    
+# Solution_2 -> 속도 빠름
+class Solution:
+    def sumOddLengthSubarrays(self, arr: List[int]) -> int:
+        return sum(((i + 1) * (len(arr) - i) + 1) // 2 * a for i, a in enumerate(arr))
