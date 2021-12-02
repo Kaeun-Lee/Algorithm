620154c498631aecd8ad3b8dfc69c1e6f011c457
devkani <lgeun7026@hanmail.net>
2021-12-02 23:56:00 +0900

    [Programmers] Update another solution

--- Programmers/Level1/키패드 누르기.py
+++ Programmers/Level1/키패드 누르기.py
@@ -2,51 +2,90 @@
 
 # Solution_1
 def solution(numbers, hand):
-    left_num = ['*', 1, 4, 7]
-    right_num = ['#', 3, 6, 9]
-    
+    left_key = {1 : (0,1), 4 : (1,1), 7 : (2,1), '*' : (3,1)} 
+    right_key = {3 : (0,3), 6 : (1,3), 9 : (2,3), '#' : (3,3)}
+    other_key = {2 : (0,2), 5 : (1,2), 8 : (2,2), 0 : (3,2)}
+
     left = []
     right = []
-
     answer = []
     
-    if hand == "left":
-        left.append('*')
-    else:
-        right.append('#')
+    left.append(left_key['*'])  # 손가락이 시작하는 key의 위치 
+    right.append(right_key['#'])
+        
     for i in numbers:
-        if i in left_num:
-            left.append(i)
+        if  i in left_key.keys():
+            left.append(left_key[i])
             answer.append('L')
-        elif i in right_num:
-            right.append(i)
+        elif i in right_key.keys():
+            right.append(right_key[i])
             answer.append('R')
-              
-        else: 
-            l = abs(i-left[-1])
-            r = abs(i-right[-1])
-            print(left)
-            print(right)
+        else:
+            l = sum([abs(x-y) for x, y in zip(left[-1], other_key[i])])   # 행렬 값을 이용하여 거리 계산
+            r = sum([abs(x-y) for x, y in zip(right[-1], other_key[i])])    
             if l < r :
-                print(l,r)
-                left.append(i)
+                left.append(other_key[i])
                 answer.append('L')
-            elif r < l :
-                print(l,r)
+            elif l > r :
+                right.append(other_key[i])
                 answer.append('R')
             else:
-                print(l,r)
-                if hand == 'left':
-                    left.append(i)
+                if hand == "left":
+                    left.append(other_key[i])
                     answer.append('L')
-                elif hand == 'right':
-                    right.append(i)
+                else:
+                    right.append(other_key[i])
                     answer.append('R')
-  
     return "".join(answer)
 
+
+# Solution_2
+def solution(numbers, hand):
+    answer = ''
+    key_dict = {1:(0,0),2:(0,1),3:(0,2),
+                4:(1,0),5:(1,1),6:(1,2),
+                7:(2,0),8:(2,1),9:(2,2),
+                '*':(3,0),0:(3,1),'#':(3,2)}
+
+    left = [1,4,7]
+    right = [3,6,9]
+    lhand = '*'
+    rhand = '#'
+    
+    for i in numbers:
+        if i in left:
+            answer += 'L'         # append 대신 문자열 합을 이용
+            lhand = i             # key만 바꿔줌
+        elif i in right:
+            answer += 'R'
+            rhand = i
+        else:
+            curPos = key_dict[i]  # i의 value 값
+            lPos = key_dict[lhand]
+            rPos = key_dict[rhand]
+            ldist = abs(curPos[0]-lPos[0]) + abs(curPos[1]-lPos[1])  # 인덱스 값으로 계산
+            rdist = abs(curPos[0]-rPos[0]) + abs(curPos[1]-rPos[1])
+
+            if ldist < rdist:
+                answer += 'L'
+                lhand = i
+            elif ldist > rdist:
+                answer += 'R'
+                rhand = i
+            else:
+                if hand == 'left':
+                    answer += 'L'
+                    lhand = i
+                else:
+                    answer += 'R'
+                    rhand = i
+    return answer
 
 
 
 test_case1 = solution([1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5], "right")
-print(test_case1)
+test_case2 = solution([7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2], "left")
+test_case3 = solution([1, 2, 3, 4, 5, 6, 7, 8, 9, 0], "right")
+print(test_case1)  # LRLLLRLLRRL
+print(test_case2)  # LRLLRRLLLRR
+print(test_case3)  # LLRLLRLLRL
