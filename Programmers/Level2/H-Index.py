1ad78e3dfaf9421968c06b282e18ec6c9c0fab08
devkani <lgeun7026@hanmail.net>
2021-12-19 23:50:59 +0900

    [Programmers] Update solution

---
+++ Programmers/Level2/H-Index.py
@@ -0,0 +1,19 @@
+# 정렬 | https://programmers.co.kr/learn/courses/30/lessons/42747
+
+# Solution_1
+from bisect import bisect_left, bisect_right
+def solution(citations):
+    answer = []
+    citations.sort()
+    
+    for i in range(max(citations), -1, -1): # 6, 5, 4, ..., 0
+        left_index = bisect_left(citations, i) # 4, 3, 3, 2, 2, 1, 0
+        right_index = bisect_right(citations, max(citations)) # 6
+        cnt = right_index - left_index # i번 이상 인용된 논문의 수
+        if cnt >= i and left_index <= i:
+            answer.append(i)
+    
+    return max(answer)
+
+test_case = solution([3, 0, 6, 1, 5])
+print(test_case) # 3
--- Programmers/Level2/오픈채팅방.py
+++ Programmers/Level2/오픈채팅방.py
@@ -7,16 +7,15 @@ def solution(record):
     
     for i in record:
         txt = i.split()
-        if txt[1] not in user:
+        if txt[1] not in user: # 사용자 id와 이름 등록
             user[txt[1]] = txt[2]
         else:
-            if txt[0] == "Enter":
-                
-                if txt[2] != user[txt[1]]:
+            if txt[0] == "Enter":               
+                if txt[2] != user[txt[1]]: # 입장 시 이름이 이미 등록된 이름과 다르다면 변경
                     user[txt[1]] = txt[2]
                 else:
                     continue
-            elif txt[0] == "Change":
+            elif txt[0] == "Change": # 이름 변경
                 user[txt[1]] = txt[2]
     
     for i in record:
@@ -25,6 +24,23 @@ def solution(record):
             answer.append(user[txt[1]]+"님이 들어왔습니다.")
         elif txt[0] == "Leave":
             answer.append(user[txt[1]]+"님이 나갔습니다.")
+            
+    return answer
+
+
+# Solution_2
+def solution(record):
+    answer = []
+    namespace = {}
+    printer = {'Enter':'님이 들어왔습니다.', 'Leave':'님이 나갔습니다.'}
+    for r in record:
+        rr = r.split(' ')
+        if rr[0] in ['Enter', 'Change']: # 입장, 변경 시 이름 등록
+            namespace[rr[1]] = rr[2]   
+    
+    for r in record:
+        if r.split(' ')[0] != 'Change':  # 이름을 변경하는 경우가 아니면, 기록함
+            answer.append(namespace[r.split(' ')[1]] + printer[r.split(' ')[0]])
             
     return answer
 
