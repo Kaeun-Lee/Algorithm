5b31760c0472d91efc24f4884c8401594896e5d4
devkani <lgeun7026@hanmail.net>
2022-01-02 23:22:11 +0900

    [Programmers] Update solution

--- Programmers/Level2/[해시] 전화번호 목록.py
+++ Programmers/Level2/[해시] 전화번호 목록.py
@@ -1,30 +1,22 @@
 # 해시 | https://programmers.co.kr/learn/courses/30/lessons/42577
 
 # Solution_1
-import re
-from collections import Counter
 def solution(phone_book):
-    lst = []
+    phone_book.sort()
     
-    for i in phone_book:
-        lst.append(len(i))
-    
-    min_ = min(lst)   
-    
-    num = []
-    for i in range(len(phone_book)):
-        num.append(phone_book[i][:min_])
-    
-
-    dic_ = {}
-    for i in num:
-        if i not in dic_:
-            dic_[i] = 1
-        else:
-            dic_[i] += 1
-    
-    for k, v in dic_.items():
-        if v >=2 :
-            return False
-        else:
+    for i in range(len(phone_book)-1):
+        s_num = phone_book[i]
+        len_ = len(s_num)
+        for j in phone_book[i+1:]:
+            if s_num in j[:len_+1]:
+                return False
             return True
+                
+
+
+test_case1 = solution(["119", "97674223", "1195524421"])
+test_case2 = solution(["123","456","789"])
+test_case3 = solution(["12","123","1235","567","88"])
+print(test_case1)
+print(test_case2)
+print(test_case3)
