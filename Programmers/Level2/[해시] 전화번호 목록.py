ce4e0012e059b390311c9a05c229221825a592c5
devkani <lgeun7026@hanmail.net>
2022-01-03 19:53:00 +0900

    [Programmers] Update solution

--- Programmers/Level2/[해시] 전화번호 목록.py
+++ Programmers/Level2/[해시] 전화번호 목록.py
@@ -13,6 +13,21 @@ def solution(phone_book):
             return True
                 
 
+# Solution_2
+def solution(phone_book):
+    answer = True
+    hash_map = {}
+    for phone_number in phone_book:
+        hash_map[phone_number] = 1
+    for phone_number in phone_book:
+        temp = ""
+        for number in phone_number:
+            temp += number
+            if temp in hash_map and temp != phone_number:
+                answer = False
+    return answer
+
+
 
 test_case1 = solution(["119", "97674223", "1195524421"])
 test_case2 = solution(["123","456","789"])
