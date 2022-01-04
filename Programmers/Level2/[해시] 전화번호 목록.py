c978eec3666b8c570a92286f10652991f6192ced
devkani <lgeun7026@hanmail.net>
2022-01-04 17:40:18 +0900

    [Programmers] Update solution

--- Programmers/Level2/[해시] 전화번호 목록.py
+++ Programmers/Level2/[해시] 전화번호 목록.py
@@ -1,31 +1,28 @@
 # 해시 | https://programmers.co.kr/learn/courses/30/lessons/42577
 
-# Solution_1
-def solution(phone_book):
-    phone_book.sort()
-    
-    for i in range(len(phone_book)-1):
-        s_num = phone_book[i]
-        len_ = len(s_num)
-        for j in phone_book[i+1:]:
-            if s_num in j[:len_+1]:
-                return False
-            return True
-                
-
-# Solution_2
+# Solution_1 -> hash 사용
 def solution(phone_book):
     answer = True
     hash_map = {}
-    for phone_number in phone_book:
+    for phone_number in phone_book:  # {'123':1, '456':1, '789':1}
         hash_map[phone_number] = 1
-    for phone_number in phone_book:
+    for phone_number in phone_book:  # '123', '456', '789'
         temp = ""
-        for number in phone_number:
+        for number in phone_number:  # '1', '2', '3'
             temp += number
             if temp in hash_map and temp != phone_number:
                 answer = False
     return answer
+
+
+# Solution_2
+def solution(phone_book):
+    phoneBook = sorted(phone_book)  # ["119", "1195524421", "97674223"]
+    
+    for p1, p2 in zip(phoneBook, phoneBook[1:]): # 119 1195524421, 1195524421 97674223
+        if p2.startswith(p1):
+            return False
+    return True
 
 
 
