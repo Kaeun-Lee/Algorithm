7e54cfb886c090b533fa7897854c441c91f4ab1c
devkani <lgeun7026@hanmail.net>
2022-01-11 23:22:17 +0900

    [Programmers] Modify the solution

--- Programmers/Level2/문자열 압축.py
+++ Programmers/Level2/문자열 압축.py
@@ -2,19 +2,40 @@
 
 # Solution_1
 def solution(s):
-    start = ''
-    cnt = 1
-    answer = ''
-    
-    for i in range(len(s)):
-        start += s[0:i+1]
-        for j in range(0, len(s), len(start)):
-            next = s[len(start)+j : len(start)+j+len(start)]
-            if next == start:
+    results = []
+
+    for i in range(1, (len(s) // 2)+1):
+        cnt = 0
+        answer = ""
+
+        for j in range(0, len(s), i):
+            if s[j:j+i] != s[j+i:j+i+i] :
+                if cnt > 0:
+                    answer += str(cnt + 1) + s[j:j+i]
+                    cnt = 0
+                else:
+                    answer += s[j:j+i]
+            else:
                 cnt += 1
-            elif next != start:
-                answer += str(cnt)
-                answer += start
-                cnt = 1
-                       
-    return answer
+
+        results.append(answer)
+
+    minimum = len(s)
+    for el in results:
+        if len(el) < minimum and len(el) != 0:
+            minimum = len(el)
+
+    return minimum
+
+
+
+test_case1 = solution("aabbaccc")
+test_case2 = solution("ababcdcdababcdcd")
+test_case3 = solution("abcabcdede")
+test_case4 = solution("abcabcabcabcdededededede")
+test_case5 = solution("xababcdcdababcdcd")
+print(test_case1) # 7
+print(test_case2) # 9
+print(test_case3) # 8
+print(test_case4) # 14
+print(test_case5) # 17
