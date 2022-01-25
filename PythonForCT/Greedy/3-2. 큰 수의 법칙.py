3d43689de5952fe4102d183819c622d798e6f124
devkani <lgeun7026@hanmail.net>
2022-01-25 22:27:56 +0900

    [PythonForCT] Create 3-2. 큰 수의 법칙.py

---
+++ PythonForCT/Greedy/3-2. 큰 수의 법칙.py
@@ -0,0 +1,55 @@
+# Greedy
+
+# Solution_1
+n, m, k = map(int, input().split())
+data = list(map(int, input().split()))
+
+data.sort()
+first = data[n - 1]
+second = data[n - 2]
+
+# 가장 큰 수가 더해지는 횟수 계산
+count = int(m / (k + 1)) * k
+count += m % (k + 1)
+
+result = 0
+result += (count) * first       # 가장 큰 수 더하기
+result += (m - count) * second  # 두 번째로 큰 수 더하기
+
+print(result)                   # 46 | 28
+
+
+# Solution_2
+# N, M, K를 공백으로 구분하여 입력받기
+n, m, k = map(int, input().split())
+# N개의 수를 공백으로 구분하여 입력받기
+data = list(map(int, input().split()))
+
+data.sort()             # 입력받은 수들 정렬하기
+first = data[n - 1]     # 가장 큰 수
+second = data[n - 2]    # 두 번째로 큰 수
+
+result = 0
+
+while True:
+    for i in range(k):  # 가장 큰 수를 K번 더하기
+        if m == 0:      # m이 0이라면 반복문 탈출
+            break
+        result += first
+        m -= 1          # 더할 때마다 1씩 빼기
+    if m == 0:          # m이 0이라면 반복문 탈출
+        break
+    result += second    # 두 번째로 큰 수를 한 번 더하기
+    m -= 1
+    
+print(result)           # 최종 답안 출력
+
+
+
+# 입력 1
+# 5 7 2
+# 2 4 3 4 6
+
+# 입력 2
+# 5 7 2
+# 3 4 3 4 3
