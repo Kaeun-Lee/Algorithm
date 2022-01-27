9b22d39ccce29e46b5d9b09641d7af33f556eab0
devkani <lgeun7026@hanmail.net>
2022-01-27 16:38:51 +0900

    [PythonForCT] Create 3-3. 숫자 카드 게임.py

---
+++ PythonForCT/Greedy/3-3. 숫자 카드 게임.py
@@ -0,0 +1,55 @@
+# Greedy
+
+# Solution_1 -> min() 함수 사용
+row, column = map(int, input().split())
+
+result = []
+for i in range(row):
+    min_num = min(list(map(int, input().split())))  # 각 행에서 가장 작은 수
+    result.append(min_num)
+    
+print(max(result))  # 2 | 3 그 중 가장 큰 수
+
+
+# Solution_2 -> Solution_1과 비슷한 풀이
+n, m = map(int, input().split())
+
+result = 0
+for i in range(n):
+    data = list(map(int, input().split()))
+    # 현재 줄에서 '가장 작은 수' 찾기
+    min_value = min(data)
+    # '가장 작은 수'들 중에서 가장 큰 수 찾기
+    result = max(result, min_value)
+    
+print(result)
+
+
+# Solution_3 -> 2중 반복문 구조를 이용
+n, m = map(int, input().split())
+
+result = 0
+# 한 줄씩 입력받아 확인
+for i in range(n):
+    data = list(map(int, input().split()))
+    # 현재 줄에서 '가장 작은 수' 찾기
+    min_value = 10001
+    for a in data:
+        min_value = min(min_value, a)
+    # '가장 작은 수'들 중에서 가장 큰 수 찾기
+    result = max(result, min_value)
+    
+print(result)
+
+
+
+# 입력1
+# 3 3
+# 3 1 2
+# 4 1 4
+# 2 2 2
+
+# 입력2
+# 2 4
+# 7 3 1 8
+# 3 3 3 4
