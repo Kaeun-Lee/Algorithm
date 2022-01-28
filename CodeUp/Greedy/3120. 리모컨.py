caf52f28f81334938a9d917f4e9cf317c8c63829
devkani <lgeun7026@hanmail.net>
2022-01-28 23:30:12 +0900

    [CodeUp] Update solution

--- CodeUp/Greedy/3120. 리모컨.py
+++ CodeUp/Greedy/3120. 리모컨.py
@@ -1,15 +1,30 @@
-# Greedy
+# Greedy | https://codeup.kr/problem.php?id=3120
 
 # Solution_1
 a, b = map(int, input().split())
 
-temperature = [10, 5, 1]
-
 cnt = 0
-start = abs(b - a)
+start = abs(a-b)
 
-for i in temperature:
-    cnt += (start // i) # 몫, 가장 큰 수로 나눠지는 몫
-    start %= i
+while True:
+    if start >= 8:                            # 8 이상인 수는 10부터 빼줌
+        start -= 10
+        cnt += 1
+    elif start >= 3 and start < 8:            # 3이상 7이하인 수는 5부터 빼줌
+        start -= 5
+        cnt += 1
+    elif abs(start) >= 1 and abs(start) < 3:  # 절댓값이 1이나 2인 경우엔 숫자 그대로 count함
+        cnt += abs(start)
+        break
+    else:                                     # 0인 경우 
+        break
+    
+print(cnt)  # 5 | 3
 
-print(cnt)  
+
+
+# 입력1
+# 7 34 
+
+# 입력2
+# 22 3
