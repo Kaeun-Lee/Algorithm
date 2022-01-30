7df0299b73998c9fe0ca530e11982dedd258ff5b
devkani <lgeun7026@hanmail.net>
2022-01-30 23:20:35 +0900

    [PythonForCT] Update solution

--- PythonForCT/Greedy/3-4. 1이 될 때까지.py
+++ PythonForCT/Greedy/3-4. 1이 될 때까지.py
@@ -5,29 +5,49 @@ n, k = map(int, input().split())
 cnt = 0
 
 while True:
-    if n % k != 0: # n이 k의 배수가 아닌 경우 1씩 빼면서 배수로 만듦
-        n -= 1
+    if n == 1:
+        break
+    if n % k == 0:
+        n //= k
         cnt += 1
     else:
-        if n // k != 1: # 몫이 1이 아닌 경우
-            n //= k
-            cnt += 1
-        else: # 몫이 1인 경우
-            cnt += 1
-            break 
-        
-print(cnt) # 3 | 2
+        n -= 1
+        cnt += 1
+print(cnt) 
 
 
-# Solution_2
+# Solution_ 2 -> 일일이 1씩 빼는 방안
+n, k = map(int, input().split())
+result = 0
+
+# N이 K 이상이라면 K로 계속 나누기
+while n >= k:
+    # N이 k로 나누어 떨어지지 않는다면 N에서 1씩 빼기
+    while n % k != 0:
+        n -= 1
+        result += 1
+    # K로 나누기
+    n //= k
+    result += 1    
+    
+# 마지막으로 남은 수에 대하여 1씩 빼기
+while n > 1:
+    n -= 1
+    result += 1
+
+print(result)
+
+
+# Solution_3 -> N이 K의 배수가 되도록 효율적으로 한 번에 빼는 방안
 n, k = map(int, input().split())
 result = 0
 
 while True:
     # (N == K로 나누어떨어지는 수)가 될 때까지 1씩 빼기
-    target = (n // k) * k # 17 // 4 = 4, 4 * 4 = 16
-    result += (n - target) # 17 - 16 = 1
-    n = target # 16
+    target = (n // k) * k   # 17 // 4 = 4, 4 * 4 = 16
+    result += (n - target)  # 17 - 16 = 1
+    n = target              # 16
+    
     # N이 K보다 작을 때(더 이상 나눌 수 없을 때) 반복문 탈출
     if n < k:
         break
@@ -41,8 +61,18 @@ print(result)
 
 
 
-# 입력1
-# 17 4
+# test_case1
+# 입력 : 17 4
+# 출력 : 3
 
-# 입력2
-# 25 5
+# test_case2
+# 입력 : 25 5
+# 출력 : 2
+
+# test_case3
+# 입력 : 25 3
+# 출력 : 6
+
+# test_case4
+# 입력 : 127 4
+# 출력 : 12
