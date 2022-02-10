3802e161b7a3876243ee8031cc5967d8aa25af67
devkani <lgeun7026@hanmail.net>
2022-02-10 17:17:25 +0900

    [LeetCode] Update solution

--- LeetCode/Easy/1989. Find_Greatest_Common_Divisor_of_Array.py
+++ LeetCode/Easy/1989. Find_Greatest_Common_Divisor_of_Array.py
@@ -1,23 +1,42 @@
 # https://leetcode.com/problems/find-greatest-common-divisor-of-array/
 
-# Solution_1
+# Solution_1 -> Time Limit Exceeded
+# class Solution:
+#     def findGCD(self, nums):
+#         min_num = min(nums)
+#         max_num = max(nums)
+        
+#         if max_num % min_num == 0: # 최댓값이 최솟값으로 나눠지는 경우, 최솟값이 최대공약수
+#             return min_num
+#         else:                      # 최댓값이 최솟값으로 나눠지지 않는 경우 최솟값을 절반으로 나누며 최솟값을 찾음
+#             tmp = min_num
+#             while tmp > 0:
+#                 tmp = min_num - 1
+#                 if  (tmp >= min_num) & (max_num % tmp == 0):
+#                     return min_num 
+#             return 1
+
+
+# Solution_2
 class Solution:
-    def findGCD(self, nums: List[int]) -> int:
+    def findGCD(self, nums):
         min_num = min(nums)
         max_num = max(nums)
         
-        
-        result = []
-        for i in range(2, 30):
-            if min_num == max_num:
-                result.append(min_num)
-                break
-            else:
-                if (min_num % i == 0) & (max_num % i == 0):
-                    result.append(i)
-                
-                elif (min_num % min_num != 0) & (max_num % max_num != 0):
-                    result.append(1)
+        if max_num % min_num == 0:
+            return min_num
+        else:
+            remain = max_num % min_num  # 나머지 값
+            while True:
+                if remain != 0 : 
+                    if min_num % remain == 0:
+                        print(min_num, remain)
+                        return remain
+                    else:
+                        min_num = remain
+                        remain = min_num % remain
+                        
+                        print(min_num, remain)
                 else:
-                    result.append(1)
-        return max(result)
+                    break   
+            return 1
