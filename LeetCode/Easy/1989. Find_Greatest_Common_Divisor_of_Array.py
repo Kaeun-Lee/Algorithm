788e4c3e817fb7ea079c618ad5e5e974e38c4d77
devkani <lgeun7026@hanmail.net>
2022-02-11 15:59:14 +0900

    [LeetCode] Update solution

--- LeetCode/Easy/1989. Find_Greatest_Common_Divisor_of_Array.py
+++ LeetCode/Easy/1989. Find_Greatest_Common_Divisor_of_Array.py
@@ -1,42 +1,41 @@
 # https://leetcode.com/problems/find-greatest-common-divisor-of-array/
-
-# Solution_1 -> Time Limit Exceeded
-# class Solution:
-#     def findGCD(self, nums):
-#         min_num = min(nums)
-#         max_num = max(nums)
-        
-#         if max_num % min_num == 0: # 최댓값이 최솟값으로 나눠지는 경우, 최솟값이 최대공약수
-#             return min_num
-#         else:                      # 최댓값이 최솟값으로 나눠지지 않는 경우 최솟값을 절반으로 나누며 최솟값을 찾음
-#             tmp = min_num
-#             while tmp > 0:
-#                 tmp = min_num - 1
-#                 if  (tmp >= min_num) & (max_num % tmp == 0):
-#                     return min_num 
-#             return 1
-
+       
+# Solution_1 -> Euclidean Algorithm
+class Solution:
+    def findGCD(self, nums):
+        a, b = min(nums), max(nums)
+        while a:             # a가 0이 되면 False
+            a, b = b % a, a  # a : 큰 수를 작은 수로 나눈 나머지, b : 작은 수
+        return b
+    
 
 # Solution_2
 class Solution:
     def findGCD(self, nums):
         min_num = min(nums)
         max_num = max(nums)
+
+        for i in range(min_num, 0, -1):  # 1씩 감소
+            if max_num % i == 0 and min_num % i == 0:
+                return i
+            
+
+# Solution_3 -> python 내장함수 중 math 모듈 사용. 속도가 더 빠름
+from math import gcd
+
+class Solution:
+    def findGCD(self, nums):
+        min_num = min(nums)
+        max_num = max(nums)
         
-        if max_num % min_num == 0:
-            return min_num
-        else:
-            remain = max_num % min_num  # 나머지 값
-            while True:
-                if remain != 0 : 
-                    if min_num % remain == 0:
-                        print(min_num, remain)
-                        return remain
-                    else:
-                        min_num = remain
-                        remain = min_num % remain
-                        
-                        print(min_num, remain)
-                else:
-                    break   
-            return 1
+        return gcd(min_num, max_num)
+
+
+
+sol = Solution()
+test_case1 = sol.findGCD([2,5,6,9,10])
+test_case2 = sol.findGCD([7,5,6,8,3])
+test_case3 = sol.findGCD([3,3])
+print(test_case1)  # 2
+print(test_case2)  # 1
+print(test_case3)  # 3
