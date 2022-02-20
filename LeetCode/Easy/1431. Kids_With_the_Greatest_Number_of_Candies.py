81187d0589149e6b1bf0fdde9ee2e85d33e02b57
devkani <lgeun7026@hanmail.net>
2022-02-20 23:51:10 +0900

    [LeetCode] Add test case

--- LeetCode/Easy/1431. Kids_With_the_Greatest_Number_of_Candies.py
+++ LeetCode/Easy/1431. Kids_With_the_Greatest_Number_of_Candies.py
@@ -6,7 +6,7 @@ class Solution:
         result = []
         max_num = 0
         for i in range(len(candies)):
-            if candies[i] > max_num:
+            if candies[i] > max_num:  # candies 중 가장 큰 수
                 max_num = candies[i]
         for j in range(len(candies)):
             if candies[j] + extraCandies >= max_num:
@@ -32,3 +32,13 @@ class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
         high_enough = max(candies) - extraCandies
         return [candy >= high_enough for candy in candies]  # 계산 수를 줄임
+    
+
+
+sol = Solution()
+test_case1 = sol.kidsWithCandies(candies = [2,3,5,1,3], extraCandies = 3)
+test_case2 = sol.kidsWithCandies(candies = [4,2,1,1,2], extraCandies = 1)
+test_case3 = sol.kidsWithCandies(candies = [12,1,12], extraCandies = 10)
+print(test_case1)  # [true,true,true,false,true] 
+print(test_case2)  # [true,false,false,false,false] 
+print(test_case3)  # [true,false,true]
