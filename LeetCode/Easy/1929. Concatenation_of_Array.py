d7394668053b961d0bb64974549b19c5b562074b
devkani <lgeun7026@hanmail.net>
2022-02-24 23:21:02 +0900

    [LeetCode] Add  test case

--- LeetCode/Easy/1929. Concatenation_of_Array.py
+++ LeetCode/Easy/1929. Concatenation_of_Array.py
@@ -17,3 +17,11 @@ class Solution:
     def getConcatenation(self, nums):
         return nums*2
         # return nums + nums
+        
+        
+
+sol = Solution()
+test_case1 = sol.getConcatenation(nums = [1,2,1])
+test_case2 = sol.getConcatenation(nums = [1,3,2,1])
+print(test_case1)  # [1,2,1,1,2,1]
+print(test_case2)  # [1,3,2,1,1,3,2,1]
