7480f48c1b405f74b542a581df0a9b3e04d8e267
devkani <lgeun7026@hanmail.net>
2022-02-25 23:31:09 +0900

    [LeetCode] Add test case

--- LeetCode/Easy/1773. Count_Items_Matching_a_Rule.py
+++ LeetCode/Easy/1773. Count_Items_Matching_a_Rule.py
@@ -1,6 +1,7 @@
 # https://leetcode.com/problems/count-items-matching-a-rule/
 
 # Solution_1 -> 속도 빠름, 메모리 차지 적음
+from typing import List
 class Solution: 
     def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
         rule = {'type' : 0, 'color' : 1, 'name' : 2}
@@ -17,3 +18,13 @@ class Solution:
     def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
         rule = {'type' : 0, 'color' : 1, 'name' : 2}
         return sum(1 for item in items if item[rule[ruleKey]] == ruleValue)
+    
+    
+ 
+sol = Solution()
+test_case1 = sol.countMatches(items = [["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]], 
+                              ruleKey = "color", ruleValue = "silver")
+test_case2 = sol.countMatches(items = [["phone","blue","pixel"],["computer","silver","phone"],["phone","gold","iphone"]],
+                              ruleKey = "type", ruleValue = "phone")
+print(test_case1)  # 1
+print(test_case2)  # 2
