3b1b5f9ec87482f2fe3c8a7dd709e989e6165342
Kaeun-Lee <lgeun7026@hanmail.net>
2022-03-03 22:44:16 +0900

    [LeetCode] Add test case

--- LeetCode/Easy/1342. Number_of_Steps_to_Reduce_a_Number_to_Zero.py
+++ LeetCode/Easy/1342. Number_of_Steps_to_Reduce_a_Number_to_Zero.py
@@ -35,3 +35,13 @@ class Solution:
             cnt += 1 if num % 2 == 0 or num == 1 else 2
             num //= 2
         return cnt
+    
+    
+    
+sol = Solution()
+test_case1 = sol.numberOfSteps(num = 14)
+test_case2 = sol.numberOfSteps(num = 8)
+test_case3 = sol.numberOfSteps(num = 123)
+print(test_case1)  # 6
+print(test_case2)  # 4
+print(test_case3)  # 12
