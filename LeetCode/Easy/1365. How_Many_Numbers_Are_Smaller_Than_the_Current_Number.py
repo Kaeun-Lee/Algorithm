71b3521432c31d226f8ff76a7bb4ae1dc5a7cd35
Kaeun-Lee <lgeun7026@hanmail.net>
2022-03-04 23:43:59 +0900

    [LeetCode] Add test case

--- LeetCode/Easy/1365. How_Many_Numbers_Are_Smaller_Than_the_Current_Number.py
+++ LeetCode/Easy/1365. How_Many_Numbers_Are_Smaller_Than_the_Current_Number.py
@@ -21,7 +21,17 @@ class Solution:
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
         dic = {}
-        for i,n in enumerate(sorted(nums)):
+        for i,n in enumerate(sorted(nums)): # [1,2,2,3,8]
             if n not in dic:
-                dic[n] = i
+                dic[n] = i                  # {0:0, 1:1, 3:2, 8:4}
         return [dic[n] for n in nums] 
+    
+    
+    
+sol = Solution()
+test_case1 = sol.smallerNumbersThanCurrent(nums = [8,1,2,2,3])    
+test_case2 = sol.smallerNumbersThanCurrent(nums = [6,5,4,8])   
+test_case3 = sol.smallerNumbersThanCurrent(nums = [7,7,7,7])   
+print(test_case1)  # [4,0,1,1,3]
+print(test_case2)  # [2,1,0,3]
+print(test_case3)  # [0,0,0,0]
