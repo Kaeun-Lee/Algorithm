6ddc4219c42656e8a30b44de03834895d5914dc6
Kaeun-Lee <lgeun7026@hanmail.net>
2022-09-14 22:55:38 +0900

    add solution

--- Programmers/Level1/신규 아이디 추천.py
+++ Programmers/Level1/신규 아이디 추천.py
@@ -22,19 +22,38 @@ def solution(new_id):
 
 # Solution_2
 import re
+def solution(new_id):
+    new_id = new_id.lower()
+    new_id = re.sub('[^a-z0-9\-_.]', '', new_id)
+    new_id = re.sub('[.]+', '.', new_id)
+    new_id = re.sub('^[.]', '', new_id)  # sub("(^\.|\.$)", "", new_id)도 가능
+    new_id = re.sub('[.]$', '', new_id)
+    if len(new_id) == 0:
+        new_id += 'a'
+    new_id = new_id[:15]
+    new_id = re.sub('[.]$', '', new_id) 
+    while len(new_id) < 3:
+        new_id +=  new_id[-1]
+    return new_id
+
+
+# Solution_3
+import re
 
 def solution(new_id):
     st = new_id
     st = st.lower()
-    st = re.sub('[^a-z0-9\-_.]', '', st)                                          # '\-'로 구분
-    st = re.sub('\.+', '.', st)                                                   # '\.+' -> . 이 1개 이상인 것은 . 하나로 치환
-    st = re.sub('^[.]|[.]$', '', st)
-    st = 'a' if len(st) == 0 else st[:15]                                         # 길이와 관련된 부분을 동시에 처리
+    st = re.sub('[^a-z0-9\-_.]', '', st)   # '\-'로 구분
+    st = re.sub('\.+', '.', st)            # '\.+' -> . 이 1개 이상인 것은 . 하나로 치환
+    st = re.sub('^[.]|[.]$', '', st)    
+    st = 'a' if len(st) == 0 else st[:15]  # 길이와 관련된 부분을 동시에 처리
     st = re.sub('^[.]|[.]$', '', st)
     st = st if len(st) > 2 else st + "".join([st[-1] for i in range(3-len(st))])  # for문으로 길이가 부족한 만큼 반복 후 리스트로 나온 값을 join
     return st
+    #  st + st[-1] * (3-len(st))도 가능
 
 
+# test case
 test_case1 = solution("...!@BaT#*..y.abcdefghijklm")
 test_case2 = solution("z-+.^.")
 test_case3 = solution("=.=")
